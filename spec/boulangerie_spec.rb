d1bbe97cf35440106475dda043a9c16b43e02d1e
Tony Arcieri <bascule@gmail.com>
2015-12-06 17:04:40 -0800

    Test initializing with a Schema class

--- spec/boulangerie_spec.rb
+++ spec/boulangerie_spec.rb
@@ -5,15 +5,27 @@ RSpec.describe Boulangerie do
     { example_key_id => "totally secret key I swear" }
   end
 
-  it "initializes a default Boulangerie" do
+  it "raises NotConfiguredError unless configured" do
     expect { described_class.default }.to raise_error(described_class::NotConfiguredError)
+  end
 
-    described_class.setup(
+  it "initializes a default Boulangerie" do
+    boulangerie = described_class.new(
       schema: fixture_path.join("example_schema.yml"),
       keys:   example_keys,
       key_id: example_key_id
     )
 
-    expect(described_class.default).to be_a described_class
+    expect(boulangerie).to be_a described_class
+  end
+
+  it "initializes from a Schema class instead of a path" do
+    boulangerie = described_class.new(
+      schema: Boulangerie::Schema.from_yaml(fixture_path.join("example_schema.yml").read),
+      keys:   example_keys,
+      key_id: example_key_id
+    )
+
+    expect(boulangerie).to be_a described_class
   end
 end
