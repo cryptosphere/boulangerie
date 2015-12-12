fd3211a86acac3624e6e0418777debfdf278e269
Tony Arcieri <bascule@gmail.com>
2015-12-12 14:07:03 -0800

    RuboCopped again

--- spec/boulangerie/keyring_spec.rb
+++ spec/boulangerie/keyring_spec.rb
@@ -18,7 +18,7 @@ RSpec.describe Boulangerie::Keyring do
 
   it "raises ArgumentError if a key is too short" do
     expect do
-      described_class.new({default_key_id => "derp"}, key_id: default_key_id)
+      described_class.new({ default_key_id => "derp" }, key_id: default_key_id)
     end.to raise_error(ArgumentError)
   end
 
