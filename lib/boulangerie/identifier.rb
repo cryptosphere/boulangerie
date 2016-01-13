b5e0f3d0c858470b89e63fdeb31da6ac563d184a
Tony Arcieri <bascule@gmail.com>
2016-01-12 20:09:32 -0800

    Clean up Boulangerie::Identifier.parse

--- lib/boulangerie/identifier.rb
+++ lib/boulangerie/identifier.rb
@@ -1,49 +1,41 @@
 class Boulangerie
   # A unique identifier per Macaroon incorporating key/schema info
   class Identifier
+    # Labels allowed in identifier headers
+    ALLOWED_LABELS = %w(v uuid kid sch iat)
+
     # Character to use for delimiting sections of the identifier
     DELIMITER = " "
 
     attr_reader :uuid, :schema_id, :schema_version, :key_id, :issued_at
 
     def self.parse(string)
-      version        = nil
-      uuid           = nil
-      schema_id      = nil
-      schema_version = nil
-      key_id         = nil
-      issued_at      = nil
+      parts = {}
 
       string.split(" ").each do |part|
-        matches = part.match(/\A([a-z]+):(.*)\z/)
+        matches = part.match(/\A(?<label>[a-z]+):(?<value>.*)\z/)
         fail SerializationError, "bad identifier: #{part}" unless matches
 
-        label = matches[1]
-        value = matches[2]
-
-        case label
-        when "v"    then version   = Integer(value, 10)
-        when "uuid" then uuid      = value
-        when "kid"  then key_id    = value
-        when "iat"  then issued_at = Time.iso8601(value)
-        when "sch"
-          matches = value.match(/\A([a-z0-9]+)@(\d+)\z/)
-          fail SerializationError, "bad schema identifier: #{value}" unless matches
-
-          schema_id      = matches[1]
-          schema_version = Integer(matches[2], 10)
-        else fail SerializationError, "unknown identifier: #{label}"
-        end
+        parts[matches[:label]] = matches[:value]
       end
 
-      fail SerializationError, "bad version: #{version.inspect}" unless version == FORMAT_VERSION
+      unless Integer(parts["v"], 10) == FORMAT_VERSION
+        fail SerializationError, "bad version: #{parts["v"].inspect}"
+      end
+
+      unless (missing_labels = ALLOWED_LABELS - parts.keys).empty?
+        fail SerializationError, "missing '#{missing_labels.first}' in identifier"
+      end
+
+      schema = parts["sch"].match(/\A(?<id>[a-z0-9]{16})@(?<version>\d+)\z/)
+      fail SerializationError, "bad schema identifier: #{parts['sch'].inspect}" unless schema
 
       new(
-        uuid:           uuid           || fail(SerializationError, "no uuid in identifier"),
-        schema_id:      schema_id      || fail(SerializationError, "no sch in identifier"),
-        schema_version: schema_version || fail(SerializationError, "no sch in identifier"),
-        key_id:         key_id         || fail(SerializationError, "no kid in identifier"),
-        issued_at:      issued_at      || fail(SerializationError, "no iat in identifier")
+        uuid:           parts["uuid"],
+        schema_id:      schema[:id],
+        schema_version: Integer(schema[:version], 10),
+        key_id:         parts["kid"],
+        issued_at:      Time.iso8601(parts["iat"])
       )
     end
 
